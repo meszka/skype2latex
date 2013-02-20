6489073ad472502d5b12ea7b9541c9a27bea2838
Jakub Szwachła <meszkax@gmail.com>
2013-02-20 17:09:16 +0100

    Add markdown escaping
    Kind of ugly... probably should make classes for latex and markdown
    escaping.

--- gen_latex.py
+++ gen_latex.py
@@ -14,7 +14,7 @@ import jinja2
 sys.stdout = codecs.getwriter(locale.getpreferredencoding())(sys.stdout)
 
 INITIALS = {u'Jakub Szwachła': u'K', u'Agnieszka Talaga': u'A'}
-ESCAPES = [('\\',  '\\textbackslash{}'),
+LATEX_ESCAPES = [('\\',  '\\textbackslash{}'),
            ('#',  '\\#'),
            ('$',  '\\$'),
            ('%',  '\\%'),
@@ -26,13 +26,16 @@ ESCAPES = [('\\',  '\\textbackslash{}'),
            ('>',  '\\textgreater{}')]
 
 
-def line_to_message(line):
+def line_to_message(line, out_format='latex'):
     match = re.search(r'\[\d\d:\d\d:\d\d] ([^:]+): (.*)', line)
     if not match:
         return None
     name, text = match.groups()
     who = INITIALS[name]
-    text = latexify(text)
+    if out_format == 'latex':
+        text = latexify(text)
+    elif out_format == 'markdown':
+        text = markdownify(text)
     text = textwrap.fill(text, width=70, break_long_words=False,
                          break_on_hyphens=False)
     message = {'who': who, 'text': text}
@@ -43,16 +46,28 @@ def latexify(text):
     new_words = []
     for word in text.split():
         if is_url(word):
-            new_word = urlize(word)
+            new_word = latex_urlize(word)
             new_words.append(new_word)
         else:
-            new_word = escape(word)
+            new_word = escape(word, LATEX_ESCAPES)
             new_words.append(new_word)
     return ' '.join(new_words)
 
 
-def escape(text):
-    for old, new in ESCAPES:
+def markdownify(text):
+    new_words = []
+    for word in text.split():
+        if is_url(word):
+            new_word = markdown_urlize(word)
+            new_words.append(new_word)
+        else:
+            new_word = re.escape(word)
+            new_words.append(new_word)
+    return ' '.join(new_words)
+
+
+def escape(text, escapes):
+    for old, new in escapes:
         text = text.replace(old, new)
     return text
 
@@ -61,8 +76,12 @@ def is_url(text):
     return re.search(r'https?://[^ ]+', text)
 
 
-def urlize(text):
+def latex_urlize(text):
     return re.sub(r'(https?://[^ ]+)', r'\\url{\1}', text)
+
+
+def markdown_urlize(text):
+    return re.sub(r'(https?://[^ ]+)', r'<\1>', text)
 
 
 def main():
@@ -76,12 +95,16 @@ def main():
     src_dir = sys.argv[1]
     template = env.get_template(sys.argv[2])
 
+    fmt = 'latex'
+    if (sys.argv[3]):
+        fmt = sys.argv[3]
+
     history = []
     for src_file in sorted(os.listdir(src_dir)):
         filename = os.path.join(src_dir, src_file)
         with codecs.open(filename, encoding='utf-8') as src:
             lines = src.readlines()
-            conversation = [line_to_message(line) for line in lines]
+            conversation = [line_to_message(line, fmt) for line in lines]
             conversation = [m for m in conversation if m is not None]
             history.append(conversation)
     print template.render(history=history)
